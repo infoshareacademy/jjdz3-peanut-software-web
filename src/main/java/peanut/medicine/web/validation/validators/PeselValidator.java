2421463fad41d95084bcaa3049e1ff8385297772
bartman4000 <bartman3000@gmail.com>
2017-05-28 11:50:57 +0200

    divided isValid()method to few methods

--- src/main/java/peanut/medicine/web/validation/validators/PeselValidator.java
+++ src/main/java/peanut/medicine/web/validation/validators/PeselValidator.java
@@ -7,6 +7,8 @@ import java.util.Arrays;
 
 public class PeselValidator implements ConstraintValidator<Pesel, String> {
 
+    private ConstraintValidatorContext constraintContext;
+
     @Override
     public void initialize(Pesel constraintAnnotation) {}
 
@@ -16,17 +18,53 @@ public class PeselValidator implements ConstraintValidator<Pesel, String> {
             return true;
         }
 
-        //check length
-        if (!object.matches("^[0-9]{11}$"))
+        if(!this.hasElevenDigits(object))
         {
             addCustomViolation("Pesel must be made of 11 digits", constraintContext);
             return false;
         }
 
-        //check date part
+        if(!hasGoodMonth(object))
+        {
+            addCustomViolation("Pesel has wrong month part", constraintContext);
+            return false;
+        }
+
+        if(!hasGoodDayOfMonth(object))
+        {
+            addCustomViolation("Pesel has wrong day of month part", constraintContext);
+            return false;
+        }
+
+        if(!hasGoodControlDigit(object))
+        {
+            addCustomViolation("Pesel has incorrect control(last) digit", constraintContext);
+            return false;
+        }
+
+        return true;
+    }
+
+    private boolean hasElevenDigits(String object)
+    {
+        return object.matches("^[0-9]{11}$");
+    }
+
+    private boolean hasGoodDayOfMonth(String object)
+    {
+        int[] februaryCodes = {82,2,22,42,62};
         int month = Integer.parseInt(object.substring(2,4));
         int day = Integer.parseInt(object.substring(4,6));
-        int[] februaryCodes = {82,2,22,42,62};
+
+        boolean isFebruary = Arrays.stream(februaryCodes).anyMatch(x -> x == month);
+        return (isFebruary && 1 <= day && day <= 28)
+                ||
+                (!isFebruary && 1 <= day && day <= 31); // one of these two conditions makes day of month valid
+    }
+
+    private boolean hasGoodMonth(String object)
+    {
+        int month = Integer.parseInt(object.substring(2,4));
 
         //check month (based on https://pl.wikipedia.org/wiki/PESEL#Data_urodzenia)
         int[] possibleMonthValues = {
@@ -44,23 +82,11 @@ public class PeselValidator implements ConstraintValidator<Pesel, String> {
                 92,12,32,52,72
         };
 
-        boolean isGoodMonth = Arrays.stream(possibleMonthValues).anyMatch(x -> x == month);
-        if(!isGoodMonth)
-        {
-            addCustomViolation("Pesel has wrong month part", constraintContext);
-            return false;
-        }
+        return Arrays.stream(possibleMonthValues).anyMatch(x -> x == month);
+    }
 
-        //check day of month
-        boolean isFebruary = Arrays.stream(februaryCodes).anyMatch(x -> x == month);
-        if (!(
-                (isFebruary && 1 <= day && day <= 28) || (!isFebruary && 1 <= day && day <= 31)) // one of this makes day of month valid
-                )
-        {
-            addCustomViolation("Pesel has wrong day of month part", constraintContext);
-            return false;
-        }
-
+    private boolean hasGoodControlDigit(String object)
+    {
         //check control digit (based on https://pl.wikipedia.org/wiki/PESEL#Cyfra_kontrolna_i_sprawdzanie_poprawno.C5.9Bci_numeru)
         int dA = Integer.parseInt(object.substring(0,1));
         int dB = Integer.parseInt(object.substring(1,2));
@@ -75,13 +101,7 @@ public class PeselValidator implements ConstraintValidator<Pesel, String> {
         int controlDigit = Integer.parseInt(object.substring(10,11));
 
         int controlSum = 9*dA + 7*dB + 3*dC + dD + 9*dE + 7*dF + 3*dG + dH + 9*dI + 7*dJ;
-        if((controlSum % 10) != controlDigit)
-        {
-            addCustomViolation("Pesel has incorrect control(last) digit", constraintContext);
-            return false;
-        }
-
-        return true;
+        return (controlSum % 10) == controlDigit;
     }
 
     private void addCustomViolation(String message, ConstraintValidatorContext constraintContext)
